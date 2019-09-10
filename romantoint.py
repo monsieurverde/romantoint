b5a478856b4e60a06442467ebff9076c9a4ff526
monsieurverde <domverdemgmt@gmail.com>
2019-09-09 21:22:56 -0400

    improved variable naming system

--- romantoint.py
+++ romantoint.py
@@ -1,25 +1,25 @@
 import sys
 import string
 
-def romanToInt(s):
+def romanToInt(roman_numeral):
        
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         prev = 0
-        t = 0
-        for i in s:
+        num = 0
+        for i in roman_numeral:
             curr = roman[i]
-            t += curr
+            num += curr
 
             if curr > prev:
-                t -= 2 * prev
+                num -= 2 * prev
             prev = curr
 
-        return t
+        return num
 
 def main():
-    x = "III"
-    test = romanToInt(x)
-    print test
+    test_numeral = "III"
+    test = romanToInt(test_numeral)
+    print(test)
 
 if __name__ == '__main__':
     main()
