8ee4b36deda2cf327216bb9092fe61a4a3758d4c
monsieurverde <domverdemgmt@gmail.com>
2019-09-17 15:08:42 -0400

    minor revisions

--- romantoint.py
+++ romantoint.py
@@ -6,8 +6,8 @@ def romanToInt(roman_numeral):
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         prev = 0
         num = 0
-        for i in roman_numeral:
-            curr = roman[i]
+        for items in roman_numeral:
+            curr = roman[items]
             num += curr
 
             if curr > prev:
